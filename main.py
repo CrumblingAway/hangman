37f884312ae6fed64fc359df61f82bcb4c186d7d
CrumblingAway <antonkid@gmail.com>
2026-09-11 20:51:30 +0300

    Loop: Outline game loop+define game messages.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import random
 
+QUIT_COMMAND = "quit"
+
+WELCOME_MSG = f"""
+\tWelcome to Hangman!
+\tTo begin press the <ENTER> key.
+\tTo make a guess press your letter of choice and then press the <ENTER> key.
+\tTo exit at any point write \"{QUIT_COMMAND}\" and press the <ENTER> key.
+"""
+
+WIN_MSG = "Congratulations! You've guessed the word!"
+LOSS_MSG = "Better luck next time!"
+
+GOODBYE_MSG = "Goodbye!"
+
 def choose_word() -> str:
     chosen_word = None
 
@@ -19,5 +33,18 @@ def choose_word() -> str:
 
     return chosen_word
 
+
+
 if __name__ == "__main__":
-    print(choose_word())
+    print(WELCOME_MSG)
+
+    user_input = input()
+    if user_input == QUIT_COMMAND:
+        print(GOODBYE_MSG)
+        quit()
+
+    word = choose_word()
+    while user_input := input() != QUIT_COMMAND:
+        pass
+
+    print(GOODBYE_MSG)
